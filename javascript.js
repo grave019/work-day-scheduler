92665cf94ea5e5b7ba49636de53a538e8f5f5860
Brent Graves <graves.brent@gmail.com>
2021-09-28 00:01:35 -0400

    added normal 9-5 business hours for the day planner in JS.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,25 @@
-$(document).ready(function(){
-    //display current day and time
-    $("#currentDay").text().format("MMMM Do YYYY, h:mm:ss a");
-    $(".saveBtn").document("click", function(){
+$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
+    //display current day & time.
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+    //Assign saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        //get nearby values.
         console.log(this);
-        let text = $(this).siblings(".description").val();
-        let time = $(this).parent().attr("id");
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        //set items in local storage.
         localStorage.setItem(time, text);
     })
+//load any saved data from LocalStorage - do this for each hour created.
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
